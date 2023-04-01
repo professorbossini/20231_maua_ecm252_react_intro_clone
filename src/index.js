ca2235f9be95bd9156ac29dd818cb1f572d8b0fc
Rodrigo Bossini <professorbossini@gmail.com>
2023-04-01 10:55:50 -0300

    apostila03(jsx): fim da apostila

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './styles.css'
 
 // JSX: JavaScript Extension (XML)
 const App = () => {
-  return <div>Meu primeiro componente React</div>
+
+  const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}
+
+  const textoDoRotulo = 'Nome:'
+
+  const obterTextoDoBotao = () => "Enviar"
+
+  const onClique = () => alert('clicou')
+
+  return <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+    <label
+      className='rotulo' 
+      htmlFor='nome' 
+      style={{display: 'block', marginBottom: 4}}>
+      {textoDoRotulo}
+    </label>
+    <input 
+      type="text" 
+      id="nome"
+      style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', outline: 'none', width: '100%', borderRadius: 8, boxSizing: 'border-box'}} />
+      <button 
+        style={estilosBotao}
+        onClick={() => onClique()}>
+        {obterTextoDoBotao()}
+      </button>
+  </div>
 }
 
 ReactDOM.render(
